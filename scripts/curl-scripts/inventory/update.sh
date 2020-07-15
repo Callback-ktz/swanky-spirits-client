428eef7f454e3b31b5be9101af1cb807b0bcf867
ako-21 <akeemogle@gmail.com>
2020-07-15 13:37:45 -0400

    Inventory and Auth Curl Scripts

---
+++ scripts/curl-scripts/inventory/update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/inventory"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+--header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "inventory": {
+      "code": "'"${CODE}"'",
+      "name": "'"${NAME}"'",
+      "unit_price": "'"${PRICE}"'",
+      "quantity": "'"${QUANT}"'"
+    }
+  }'
+
+echo

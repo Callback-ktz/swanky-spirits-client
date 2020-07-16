1f023f9e112e0bc4166b744ad3313a617981a91b
ako-21 <akeemogle@gmail.com>
2020-07-16 18:29:56 -0400

    Added delete button to each inventory item
    User is able to click delete button, and inventory item will remove
    right away

--- src/components/InventoryIndex.js
+++ src/components/InventoryIndex.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 import axios from 'axios'
 import apiUrl from './../apiConfig.js'
 
 class InventoryIndex extends React.Component {
   state= {
     inventory: null
+  }
+
+  deleteItem = (event) => {
+    event.persist()
+    console.log('someone done clicked me!', this.props, event.target.id, this.state)
+    axios({
+      method: 'DELETE',
+      url: apiUrl + '/inventory/' + event.target.id,
+      headers: {
+        'Authorization': `Bearer ${this.props.user.token}`
+      }
+    })
+      .then(response => {
+        this.setState({
+          inventory: [...this.state.inventory.filter(item => item._id !== event.target.id)]
+        })
+      })
+      .catch(console.error)
   }
 
   componentDidMount () {
@@ -35,26 +54,34 @@ class InventoryIndex extends React.Component {
       inventoryJSX = <p>No items currently in inventory</p>
     } else {
       inventoryJSX = (
-        <Table responsive="md" striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>Code</th>
-              <th>Name</th>
-              <th>Unit Price</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.inventory.map(item => (
-              <tr key={item._id}>
-                <td>{item.code}</td>
-                <td>{item.name}</td>
-                <td>{item.unit_price}</td>
-                <td>{item.quantity}</td>
+        <React.Fragment>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Code</th>
+                <th>Name</th>
+                <th>Unit Price</th>
+                <th>Quantity</th>
+                <th width={'10%'}></th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {this.state.inventory.map(item => {
+                return (
+                  <React.Fragment key={item.code}>
+                    <tr>
+                      <td>{item.code}</td>
+                      <td>{item.name}</td>
+                      <td>{item.unit_price}</td>
+                      <td>{item.quantity}</td>
+                      <td align="center"><Button id={item._id} onClick={this.deleteItem} variant="outline-danger" size="sm">Delete</Button></td>
+                    </tr>
+                  </React.Fragment>
+                )
+              })}
+            </tbody>
+          </Table>
+        </React.Fragment>
       )
     }
     return (

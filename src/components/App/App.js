a8c0654575c4b6709c759db590a20c1c465efa13
austintorres <torresgaustin@gmail.com>
2020-07-16 13:27:07 -0400

    InventoryIndex additions
    Added table
    Need to style mobile view
    Added Inventory link to header.js
    Fixed AuthenticatedRoute in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       user: null,
       msgAlerts: []
     }
-    this.setUser = this.setUser.bind(this)
+    // this.setUser = this.setUser.bind(this)
   }
 
   setUser = user => this.setState({ user })
@@ -56,7 +56,7 @@ class App extends Component {
           <AuthenticatedRoute user={user} path='/change-password' render={() => (
             <ChangePassword msgAlert={this.msgAlert} user={user} />
           )} />
-          <Route user={user} path='/inventory' render={() => (
+          <AuthenticatedRoute user={user} path='/inventory' render={() => (
             <InventoryIndex msgAlert={this.msgAlert} user={user} />
           )} />
         </main>
